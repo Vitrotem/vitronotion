51398285fda4ce8387708ac36b18f253c964fa71
sirthomasii <tom.grandsaert@gmail.com>
2022-02-13 20:21:36 +0100

    asdf

--- JS/header.js
+++ JS/header.js
@@ -2,6 +2,9 @@ $( document ).ready(function() {
   	
 	$( "iframe" ).setAttribute("id", "test");
 
-});
+	var e = jQuery.Event("click");
+	e.ctrlKey = true;
+	$('a').trigger(e);
 
-$('a').trigger($.Event("click", { ctrlKey: true }));
+
+});
